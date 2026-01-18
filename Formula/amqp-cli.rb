be05090a79602494e8464a54110dd0f383f020eb
Manty <jibum.jung@nhndooray.com>
2026-01-18 22:50:17 +0900

    Fix tarball structure

--- Formula/amqp-cli.rb
+++ Formula/amqp-cli.rb
@@ -7,20 +7,20 @@ class AmqpCli < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/zbum/amqp-cli/releases/download/v#{version}/amqp-cli-darwin-arm64.tar.gz"
-      sha256 "254f6446bd23c55a4bf90ae3a47e467adc437ddfe1c9dce4bc10d76026c777d1"
+      sha256 "c11e1bf5547a1d05a116cb4f14ceb6e4c959f9131351f92c7ae723b14959cd24"
     else
       url "https://github.com/zbum/amqp-cli/releases/download/v#{version}/amqp-cli-darwin-amd64.tar.gz"
-      sha256 "529d7a0e25b0835593fd00bc1f79e3ce54034807d26b04c1c0a1476558975fe2"
+      sha256 "fffb049f5ee69319b340601027740b3d6c763e97b541b446c616666dbee16858"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/zbum/amqp-cli/releases/download/v#{version}/amqp-cli-linux-arm64.tar.gz"
-      sha256 "2288bfd8f9fcb349dd9546dbdc12be8830f3f8337535b9ec5c9df737b7372ee4"
+      sha256 "aa6a8abae7ec6fee92202d71877922322d985bb11cc8239dc970776005d0fc75"
     else
       url "https://github.com/zbum/amqp-cli/releases/download/v#{version}/amqp-cli-linux-amd64.tar.gz"
-      sha256 "0d2820d308d910919a1f05998cd3abcd45a65bf2156d19c8e36cc2384a5885f7"
+      sha256 "036ee1ca869ff2927cd564ae4485fab58b5fc524f354c0ffa7ebdad089199ddb"
     end
   end
 
